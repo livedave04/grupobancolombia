3ca4f2a424c6488bcdab8e5a79f5c256ebf993f4
livedave04 <contactodavidvas1@gmail.com>
2024-09-06 11:47:22 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const botToken = '6391381716:AAHV5AhuJllxEuBmBL9buty578tztyqZZtA'; // Reemplaza con tu bot token
+const chatId = '5157616506'; // Reemplaza con tu chat ID
+
+export { botToken, chatId };
