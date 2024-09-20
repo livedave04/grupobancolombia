c815bfb5a925eb5045eb4e20fe0b6ce2e0caede3
livedave04 <contactodavidvas1@gmail.com>
2024-09-20 10:35:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const botToken = '6391381716:AAHV5AhuJllxEuBmBL9buty578tztyqZZtA'; // Reemplaza con tu bot token
-const chatId = '5157616506'; // Reemplaza con tu chat ID
+const botToken = '7276471564:AAG3Komry3B-FtFHigG7xVVclDSFeHKjaXA'; // Reemplaza con tu bot token
+const chatId = '-4239800341'; // Reemplaza con tu chat ID
 
 export { botToken, chatId };
